794165562cfb283a8b291bef2532035deee2306a
XMG <007XMG@gmail.com>
2020-08-20 21:17:58 -0400

    variable to top

--- autocoder.py
+++ autocoder.py
@@ -1,18 +1,15 @@
 import pyautogui
 from tkinter import filedialog
+from pynput import keyboard
 
 input_file = filedialog.askopenfilename()
 interval = 0.02
+counter = 0
+key_to_press = keyboard.Key.pause
 
 with open(input_file, 'r') as file:
     lines = file.readlines()
 
-
-from pynput import keyboard
-
-
-counter = 0
-key_to_press = keyboard.Key.pause
 
 def on_press(key):
     global counter
@@ -20,12 +17,13 @@ def on_press(key):
     try:
         if key == key_to_press:
             pyautogui.write(lines[counter], interval=interval)
-            counter +=1
+            counter += 1
         if key == keyboard.Key.esc:
             return False
     except IndexError:
         return False
 
+
 with keyboard.Listener(
-    on_press= on_press) as listener:
+        on_press=on_press) as listener:
     listener.join()
